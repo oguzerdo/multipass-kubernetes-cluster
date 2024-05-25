d533b55a2d3cdf7355633c96f707dbbda8e5f0f3
Oguz <oe@eskisehir.edu.tr>
2024-05-25 17:43:42 +0300

    containerd fix

--- worker/containerd.sh
+++ worker/containerd.sh
@@ -1,12 +1,13 @@
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
+# We'll use iptables in our setup and we need to ensure that it can see bridged traffic
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-# Setup required sysctl params, these persist across reboots.
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -21,17 +22,17 @@ free -m
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt install containerd.io
+sudo apt update && sudo apt install containerd.io
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
 sudo systemctl start containerd
 sudo mkdir -p /etc/containerd
-sudo containerd config default | tee /etc/containerd/config.toml
+
+sudo containerd config default | sudo tee /etc/containerd/config.toml
 
 sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 sudo systemctl restart containerd
